fa9f525d51e5e4ce0ed1f654face377454ee1de7
DrewMorgz <andym0rgan@icloud.com>
2026-06-01 18:39:57 -0400

    Remove root index.tsx - src/index.tsx is correct entry point

--- index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import AffinityCore from "./affinity_core_unified_v3";
-
-const container = document.getElementById("root");
-if (container) {
-  createRoot(container).render(<AffinityCore />);
-}
